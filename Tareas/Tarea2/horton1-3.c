e554bfdec540e88f2d6dbe0141bb963ad356c918
Juan Andrés Urrea <ja.urrea10@uniandes.edu.co>
2015-06-06 00:35:39 -0500

    Añadir horton1-3.c
    Se añade el código y comentarios para horton1-3.c

---
+++ Tareas/Tarea2/horton1-3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+     * Se crea el printf para mostrar el mensaje, introduciendo los caracteres especiales con backslash para comillas y apóstrofe.
+     */
+    printf("\"It\'s freezing in here,\" he said coldly.\n");
+    return 0;
+}
