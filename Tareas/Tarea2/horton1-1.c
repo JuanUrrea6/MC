869ecbac231e021dd60a88cdfa054c6b0ebe55df
Juan Andrés Urrea <ja.urrea10@uniandes.edu.co>
2015-06-06 00:25:04 -0500

    Añadir comentarios a código
    Se comenta lo realizado en horton1-1.c

--- Tareas/Tarea2/horton1-1.c
+++ Tareas/Tarea2/horton1-1.c
@@ -2,6 +2,9 @@
 
 int main(void)
 {
+    /*
+     * Se crean dos printf para imprimir nombre y dirección, creando una nueva línea después
+     */
     printf("Juan Andrés Urrea Niño\n");
     printf("Evergreen Terrace 123\n");
     return 0;
