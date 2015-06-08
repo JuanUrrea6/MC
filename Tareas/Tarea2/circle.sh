75aa8d6640f26285b8462d5a2da0b836f23cf321
Juan Andrés Urrea <ja.urrea10@uniandes.edu.co>
2015-06-07 20:00:46 -0500

    Comentar circle.sh
    Se añaden los comentarios para circle.sh

--- Tareas/Tarea2/circle.sh
+++ Tareas/Tarea2/circle.sh
@@ -3,15 +3,24 @@
 
 #Se inicia el uso de GNUPlot
 gnuplot << EOF
+    #Se define el tamaño de la gráfica.
     set size square 1,1
+    #Se quita la leyenda.
     set key off
+    #Se quitan las marcas de los ejes.
     unset xtics
     unset ytics
+    #Se quitan los ejes.
     unset border
+    #Se define la salida en la terminal.
     set term dumb
+    #Se programa la gráfica como paramétrica.
     set parametric
+    #Se define el rango de ambos ejes
     set xrange[-1:1]
     set yrange[-1:1]
+    #Se situa el centro en 0,0
     set origin 0.0,0.0
+    #Se muestra la gráfica.
     plot $1*sin(t),$1*cos(t)
 EOF
