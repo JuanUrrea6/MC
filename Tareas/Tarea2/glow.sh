0795db026fc802ab2b32e2e96a33e9bd1c8b5bc7
Juan Andrés Urrea <ja.urrea10@uniandes.edu.co>
2015-06-08 16:18:04 -0500

    Corrección de glow.sh
    Se cambia el código de glow.sh para que recurra a circle.sh en la
    creación de círculos con radio dado.

--- Tareas/Tarea2/glow.sh
+++ Tareas/Tarea2/glow.sh
@@ -1,38 +1,22 @@
 #!/bin/bash
 #Método para presentar círculos de radio variable.
 
-#Se crea un radio distinto según la función dada.
-gnuplot << EOF
-    #Se define el tamaño de la gráfica.
-    set size square 1,1
-    #Se quita la leyenda.
-    set key off
-    #Se quitan las marcas de los ejes.
-    unset xtics
-    unset ytics
-    #Se quitan los ejes.
-    unset border
-    #Se define la salida en la terminal.
-    set term dumb
-    #Se programa la gráfica como paramétrica.
-    set parametric
-    #Se define el rango de ambos ejes
-    set xrange[-1:1]
-    set yrange[-1:1]
-    #Se situa el centro en 0,0
-    set origin 0.0,0.0
-    #Se declaran las variables de condición.
-    i = 0
-    t = 0
-    #Se inicia el bucle.
-    while (t == 0){
-        #Se limpia la terminal
-        clear
-        #Se crea la función de radio.
-        r = (cos(800*i))**2
-        #Se presenta la gráfica y se cambian las variables.
-        plot r*sin(t), r*cos(t)
-        i = i + 0.001
-        pause 1
-    }
-EOF
+#Se crea la variable iterante.
+x=0
+y=0
+
+#Se crea el loop infinito.
+while (y==0)
+do
+#Se limpia la terminal.
+clear
+#Se usa bc para calcular el coseno cuadrado de la variable iterante.
+r1=$(echo "(c($x))^2" | bc -l)
+#Se utiliza circle.sh para generar el círculo.
+./circle.sh $r1
+#Se esperan 0.3 segundos entre cada iteración.
+sleep 0.3
+#Se avanza en la variable iterante.
+x=$(($x+1))
+done
+
