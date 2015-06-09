4639b23a316b3809bed8cdb1d202a988d51ea1c8
Juan Andrés Urrea <ja.urrea10@uniandes.edu.co>
2015-06-09 09:06:33 -0500

    Comentarios de trozo.sh
    Se añaden los comentarios para trozo.sh

--- Tareas/Tarea2/trozo.sh
+++ Tareas/Tarea2/trozo.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #Método para recuperar un trozo dado posiciones de incio y final.
+#Se declara la primera posición tomando en cuenta la posicion 0.
 lim1=$(($1 - 1))
+
+#Se declara el número de caracteres a partir de la posición de inicio que se desean.
 lim2=$(($2 - $1 + 1))
+
+#Se obtiene el string que está contenido en el archivo.
 archivo=$(cat $3)
+
+#Se extrae el trozo del archivo deseado.
 echo ${archivo:lim1:lim2}
